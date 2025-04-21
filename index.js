eba9dd6611790cfbccc7fedcc731d46f22240def
amr0mengd <amrmengd0@protonmail.com>
2025-04-21 11:44:03 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let fullName = "AMR MONGED";
+let age = 20;
+let favoritefood = "pizza";
+
+document.getElementById("name").textContent = `your name is ${fullName}`;
+document.getElementById("age").textContent = `your age is ${age}`;
+document.getElementById("food").textContent = `your favorite food is ${favoritefood}`;
