81f434900d001aaf0f0a0d5ef098cb17a8c2c5a5
amr0mengd <amrmengd0@protonmail.com>
2025-04-21 11:45:42 +0200

    commit

--- index.js
+++
@@ -1,7 +0,0 @@
-let fullName = "AMR MONGED";
-let age = 20;
-let favoritefood = "pizza";
-
-document.getElementById("name").textContent = `your name is ${fullName}`;
-document.getElementById("age").textContent = `your age is ${age}`;
-document.getElementById("food").textContent = `your favorite food is ${favoritefood}`;
