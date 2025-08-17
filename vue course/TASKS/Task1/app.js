36b41cc7c74d93bfc4b7a40753cf7dd328dbca85
amr0mengd <amrmengd0@protonmail.com>
2025-08-17 23:19:19 +0300

    just added some extra code to clarify for my self

--- vue course/TASKS/Task1/app.js
+++ vue course/TASKS/Task1/app.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
       Name: "AmrMonged",
       Age: 20,
       picLink: 'https://i.pinimg.com/1200x/85/95/cd/8595cdd5af00cdac149d861ba9e8fcce.jpg',
+      rad: `RandomNum3: ${Math.random().toFixed(4)}`,
     };
   },
   methods: {
