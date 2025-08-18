2f45179b73fec838c8893faa2d0b84c6b72dbfde
amr0monged <amrmengd0@protonmail.com>
2025-08-18 22:25:28 +0300

    ended the 3rd project and preparing for the 2nd assignment

--- vue course/projects/drei/app.js
+++ vue course/projects/drei/app.js
@@ -2,9 +2,21 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmedName: "",
     };
   },
   methods: {
+    confirmed() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      // event.preventDefault();
+      alert("Submitted");
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
     increase(num) {
       this.counter += num;
     },
