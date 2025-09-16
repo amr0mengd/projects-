1725d06871c19b8d1868ca38c56bf14dc18e1bbf
amr0monged <amrmengd0@protonmail.com>
2025-09-16 23:23:15 +0300

    done with second cli vue project

---
+++ vue course/projects/third-cli/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <section>
+    <TheHeader />
+    <!-- <the-header></the-header> -->
+
+    <BadgeList />
+    <!-- <badge-list></badge-list> -->
+
+    <UserInfo
+      :full-name="activeUser.name"
+      :role="activeUser.role"
+      :info-text="activeUser.description"
+    />
+    <!-- <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info> -->
+  </section>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+
+export default {
+  components: {
+    // 'the-header': TheHeader
+    // TheHeader: TheHeader
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: "Amr Monged",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
