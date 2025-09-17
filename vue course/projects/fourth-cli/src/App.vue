e841bd7a7c613f295b583867c854e397e9e391eb
amr0monged <amrmengd0@protonmail.com>
2025-09-17 11:10:53 +0300

    done with the fourth cli project related to dynamic components

---
+++ vue course/projects/fourth-cli/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <button @click="selectedComponent('active-goals')">Active Goals</button>
+    <button @click="selectedComponent('manage-goals')">Manage Goals</button>
+    <keep-alive>
+      <component :is="selectedComponents"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+
+export default {
+  components: { TheHeader, ActiveGoals, ManageGoals },
+  data() {
+    return {
+      selectedComponents: "active-goals",
+    };
+  },
+  methods: {
+    selectedComponent(cmp) {
+      this.selectedComponents = cmp;
+    },
+  },
+};
+</script>
