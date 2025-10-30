1e1035a62b33cb197149853fce139387e233af18
amr0monged <amrmengd0@protonmail.com>
2025-10-30 09:32:20 +0300

    done with coach detail comp and started the filtering comp

--- vue course/CourseProjects/find-coach-project/src/pages/coaches/CoachDetail.vue
+++ vue course/CourseProjects/find-coach-project/src/pages/coaches/CoachDetail.vue
@@ -1,7 +1,53 @@
 <template>
-  <section>
-    Details for Coach
-    <router-view></router-view>
-    <router-link to="/coaches/c1/contact">Contact</router-link>
-  </section>
+  <div>
+    <section>
+      <base-card>
+        <h2>{{ fullName }}</h2>
+        <h3>${{ selectedCoach.hourlyRate }}/hour</h3>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <header>
+          <h2>Interested? Reach out Now!</h2>
+          <base-button link :to="contactLink">Contact</base-button>
+        </header>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <base-badge
+          v-for="area in selectedCoach.areas"
+          :key="area"
+          :type="area"
+          :title="area"
+        ></base-badge>
+        <p>{{ selectedCoach.description }}</p>
+      </base-card>
+    </section>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ["id"],
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+  created() {
+    this.selectedCoach = this.$store.getters["coaches/coaches"].find(
+      (coach) => coach.id === this.id
+    );
+  },
+  computed: {
+    fullName() {
+      return this.selectedCoach.firstName + " " + this.selectedCoach.lastName;
+    },
+    contactLink() {
+      return this.$route.path + "/" + this.id + "/contact";
+    },
+  },
+};
+</script>
