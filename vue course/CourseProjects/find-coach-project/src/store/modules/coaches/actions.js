6d3d4bfb65762c3cda9c3b1890c9ab5b1c072c0f
amr0mengd <amrmengd0@protonmail.com>
2025-11-01 13:55:37 +0200

    beginning to use a backend server with vuex (stopped at for loop)

--- vue course/CourseProjects/find-coach-project/src/store/modules/coaches/actions.js
+++ vue course/CourseProjects/find-coach-project/src/store/modules/coaches/actions.js
@@ -1,13 +1,38 @@
 export default {
-  registerCoach(context, payload) {
+  async registerCoach(context, payload) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: payload.first,
       lastName: payload.last,
       description: payload.desc,
       hourlyRate: payload.rate,
       areas: payload.areas,
     };
+    const response = await fetch(
+      `https://vue-http-demo-b6bc0-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify(coachData),
+      }
+    );
+    // const responseData = await response.json();
+    if (!response.ok) {
+      //error....
+    }
     context.commit("registerCoach", coachData);
+  },
+  async loadCoaches(context, payload) {
+    const response = await fetch(
+      `https://vue-http-demo-b6bc0-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      //....
+    }
+
+    const coaches = [];
+    for (const key in responseData){
+      
+    }
   },
 };
