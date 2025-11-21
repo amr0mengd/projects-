42b3c2dba882ea8c8a2c645af9337f7ea94a8368
amr0mengd <amrmengd0@protonmail.com>
2025-11-21 09:20:20 +0200

    in progress with pinia (with a bug now)

--- vue course/Pinia-Project/pinia-project/src/App.vue
+++ vue course/Pinia-Project/pinia-project/src/App.vue
@@ -5,9 +5,23 @@
       <img src="./assets/pinia-logo.svg" alt="pinia logo" />
       <h1>Pinia Tasks</h1>
     </header>
+    <!-- task list -->
+    <div class="task-list">
+      <div v-for="task in taskStore" :key="task.id">
+        <task-details :task="task"></task-details>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import TaskDetails from "./components/TaskDetails.vue";
+import { useTaskStore } from "./stores/TaskStore";
+export default {
+  components: { TaskDetails },
+  setup() {
+    const taskStore = useTaskStore();
+    return { taskStore };
+  },
+};
 </script>
--- vue course/Pinia-Project/pinia-project/src/main.js
+++ vue course/Pinia-Project/pinia-project/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "./assets/main.css";
+import { createPinia } from "pinia";
+
+const app = createApp(App);
+
+app.use(createPinia());
+
+app.mount("#app");
